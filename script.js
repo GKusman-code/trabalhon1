f22c3d45566b307fa76dfbcfefe9c5e5c8f1132e
GKusman-code <gabrielkusman3334@gmail.com>
2025-07-07 01:47:37 -0300

    adição de novos arquivos

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function mostrarMensagem() {
   const mensagem = document.getElementById("mensagem");
+
   mensagem.innerHTML = `
-    <span>Rumo ao espaço com a tecnologia!</span><br><br>
-    <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" alt="Foguete" width="60">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Earth_Western_Hemisphere.jpg/320px-Earth_Western_Hemisphere.jpg" alt="Terra" width="60">
+    <div style="text-align: center;">
+      <img src="https://i.gifer.com/7efs.gif" alt="Foguete decolando" width="150">
+    </div>
   `;
+
+  // Rola suavemente até a mensagem
+  mensagem.scrollIntoView({ behavior: 'smooth' });
 }
