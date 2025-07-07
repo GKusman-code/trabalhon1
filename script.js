0f5aa3b7cb3e9575b227c13f97486097bce1323b
GKusman-code <gabrielkusman3334@gmail.com>
2025-07-07 02:15:15 -0300

    adição de novos arquivos

--- script.js
+++ script.js
@@ -3,9 +3,7 @@ function mostrarMensagem() {
 
   mensagem.innerHTML = `
     <p>Rumo ao espaço com a tecnologia!</p>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Emoji_u1f680.svg/120px-Emoji_u1f680.svg.png" 
-         alt="Foguete decolando" 
-         class="foguete">
+    <img src="imagem foguete/imagem foguete.png" alt="Foguete" class="foguete">
   `;
 
   mensagem.scrollIntoView({ behavior: 'smooth' });
