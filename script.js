c2fbeec3107d2b342449bf2b6edde385ce8bf22f
GKusman-code <gabrielkusman3334@gmail.com>
2025-07-07 01:36:24 -0300

    adição de novos arquivos

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function mostrarMensagem() {
   const mensagem = document.getElementById("mensagem");
-  mensagem.textContent = "Você é capaz de conquistar tudo que quiser! 🚀";
+  mensagem.innerHTML = `
+    <span>Rumo ao espaço com a tecnologia!</span><br><br>
+    <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" alt="Foguete" width="60">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Earth_Western_Hemisphere.jpg/320px-Earth_Western_Hemisphere.jpg" alt="Terra" width="60">
+  `;
 }
